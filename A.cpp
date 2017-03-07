f264c5bccd8ce0ef055645247bba1fb545a9ee77
cutechopper1224 <cutechopper1224@gmail.com>
2017-03-07 21:05:28 +0800

    A.cpp

---
+++ A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+        int n;
+      cin >> n;
+ while(n != 1)
+{
+       cout << n << " ";
+   if(n % 2 == 1) n = 3 * n + 1;
+       else n = n / 2;
+
+}
+
+   return 0;
+}
